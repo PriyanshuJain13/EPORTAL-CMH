007bbd2c703187b4e4d75501300b6a3cc1f48ba9
PriyanshuJain13 <pjstlyes13@gmail.com>
2023-09-23 20:30:00 +0530

    finalised register form

--- client/src/Components/RegisterForm.jsx
+++ client/src/Components/RegisterForm.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
-
+import bgImage from "../assets/LandingBg.png";
 const RegisterForm = () => {
+  let bgStyle = {
+    height: "100vh",
+    width: "100vw",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundImage: `url(${bgImage})`,
+  };
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
@@ -37,36 +45,40 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className='absolute top-1/4 left-1/3 border border-sky-500 m-4 p-8 w-1/4'>
-      <h2 className='text-center mb-10 text-4xl text-sky-500'>Register</h2>
+    <div style={bgStyle}>
+    <div className='absolute mt-44 left-1/3 border m-4 p-8 w-1/3 bg-black bg-opacity-30 rounded-lg'>
+      <h2 className='text-center mb-10 text-5xl font-bold text-white'>Register</h2>
       <form className='space-y-5'>
         <div className='flex flex-col space-y-2'>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} placeholder="Enter your username" className='border border-slate-500 rounded-md p-2'/>
+          <label className='text-white text-3xl'>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} placeholder="Enter your username" className='border border-slate-500 rounded-md p-2 bg-white bg-opacity-20 text-white'/>
         </div>
         <div className='flex flex-col space-y-2'>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" className='border border-slate-500 rounded-md p-2'/>
+          <label className='text-white text-3xl'>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" className='border border-slate-500 rounded-md p-2 bg-white bg-opacity-20 text-white'/>
         </div>
         <div className='flex flex-col space-y-2'>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} placeholder="Enter your Email" className='border border-slate-500 rounded-md p-2'/>
+          <label className='text-white text-3xl'>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} placeholder="Enter your Email" className='border border-slate-500 rounded-md p-2 bg-white bg-opacity-20 text-white'/>
         </div>
         <div >
-          <label>Role:</label>
-          <select value={role} onChange={handleRoleChange} className = "">
+          <label className='text-white text-3xl mr-3'>Role:</label>
+          <select value={role} onChange={handleRoleChange} className = "p-2 rounded-lg">
             <option value="judge">Judge</option>
             <option value="lawyer">Lawyer</option>
             <option value="litigant">Litigant</option>
             <option value="admin">Admin</option>
           </select>
           <div>
-          <a href='/login' className='text-blue-800 text-sm'>Already Registered?</a>
-
           </div>
         </div>
+        <div>
+        <a href='/login' className='text-blue-800 text-md'>Already Registered?</a>
+        </div>
+        
         <button type="button" onClick={handleRegister} className = "bg-sky-500 p-3 rounded-lg text-white">Register</button>
       </form>
+    </div>
     </div>
   );
 };
