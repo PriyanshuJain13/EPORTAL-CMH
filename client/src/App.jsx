5379fde1808564b4279c56def956aee546d10753
PriyanshuJain13 <pjstlyes13@gmail.com>
2023-09-23 13:20:21 +0530

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
-import React , {useState} from 'react';
+import React, { useState } from "react";
 import "./App.css";
 import LoginForm from "./Components/LoginForm";
-import RegisterForm from './Components/RegisterForm';
+import RegisterForm from "./Components/RegisterForm";
+import MainPage from "./Components/MainPage";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
-  const [currentFrom,setCurrentForm] = useState('l')
+  const [currentFrom, setCurrentForm] = useState("l");
+  const [IsRegistered, setIsRegistered] = useState(false);
+
   return (
     <>
-      <div>
-        {
-          currentFrom === 'login'? <LoginForm />:<RegisterForm />
-        }
-
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<MainPage/>}></Route>
+          <Route path = "/login" element = {<LoginForm/>}></Route>
+          <Route path = "/register" element = {<RegisterForm/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <MainPage /> */}
+      {/* <div>{currentFrom === "login" ? <LoginForm /> : <RegisterForm />}</div> */}
     </>
   );
 }
---
+++ client/src/Components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+      <>
+        <div className='text-slate-950'>MainPage</div>
+        <a href="/login">Login</a>
+        <a href='/register'>Register</a>
+      </>
+    
+  )
+}
+
+export default MainPage
