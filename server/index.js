64d69ffe4f4cd25c3cce75cac4b9cc76f8d921a3
Anshul Maurya <87333511+anshul-maurya@users.noreply.github.com>
2023-09-23 13:57:49 +0530

    intial commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log("hii");
+const express = require("express");
+
+const app = express();
+
+const http = require("http");
+
+const server = http.createServer(app);
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log("Server is running"));
